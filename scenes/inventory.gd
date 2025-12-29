a2039d0a29d6da4d59e131053d6354eea6ced561
Guy Geva <guygeva2311@gmail.com>
2025-12-29 20:40:35 +0200

    Guardian

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -4,14 +4,37 @@ class_name Inventory
 
 @export var item_type: Item = null
 @export var amount = 0
+@export var capacity: int = -1 # -1 means infinite capacity
 
 signal updated()
 
-func try_add(item_type_: Item, amount_: int):
-    if self.item_type == null or self.item_type == item_type_:
-        self.item_type = item_type_
-        self.amount += amount_
-        emit_signal("updated")
-        return true
-    
-    return false
+func try_add(item_type_: Item, amount_: int) -> int:
+	if capacity != -1 and self.amount + amount_ > capacity:
+		amount_ = capacity - self.amount
+		assert(amount_ >= 0)
+
+	if self.item_type == null or self.item_type == item_type_:
+		self.item_type = item_type_
+		self.amount += amount_
+		self.updated.emit()
+		return amount_
+	
+	return 0
+
+
+func try_transfer_to(target_inventory: Inventory, amount_: Variant) -> bool:
+	if amount_ == null:
+		amount_ = self.amount
+	assert(amount_ is int)
+	if self.item_type == null or self.amount < amount_:
+		return false
+	
+	var transfered_amount = target_inventory.try_add(self.item_type, amount_)
+	if transfered_amount > 0:
+		self.amount -= transfered_amount
+		if self.amount == 0:
+			self.item_type = null
+		self.updated.emit()
+		return true
+	
+	return false
---
+++ scenes/tiles/guardian.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+@export var inventory: Inventory
+@export var needed_item: Item
+
+@onready var target = $Target
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	assert(self.needed_item != null)
+	assert(self.inventory != null)
+	target.on_reached.connect(self._activate)
+
+func _activate(player: Player) -> void:
+	if player.inventory.item_type == self.needed_item:
+		if player.inventory.try_transfer_to(self.inventory, null):
+			print("Guardian accepted the", self.needed_item.name)
+		else:
+			print("Failed to transfer item to guardian's inventory")
+	else:
+		print("Player does not have the needed item:", self.needed_item.name)
