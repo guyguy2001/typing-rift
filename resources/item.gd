b016bbc6dfa77c2408113cef4840f0dabdff13b1
guyguy2001 <guygeva2311@gmail.com>
2026-01-03 21:34:38 +0200

    WIP

--- resources/item.gd
+++ resources/item.gd
@@ -2,5 +2,13 @@ extends Resource
 
 class_name Item
 
+enum ITEM_TYPE {
+	COPPER,
+	IRON,
+	STEEL,
+	WOOD,
+}
+
 @export var icon: Texture = null
 @export var name: String = ""
+@export var id: ITEM_TYPE
--- scenes/game2/building_data.gd
+++ scenes/game2/building_data.gd
@@ -2,6 +2,14 @@ extends Resource
 
 class_name BuildingData
 
+enum ITEM_TYPE {
+	COPPER,
+	IRON,
+	STEEL,
+	WOOD,
+}
+
 @export var name: String
 @export var scene: PackedScene
 @export var icon: Texture2D
+@export var cost: Dictionary[ITEM_TYPE, int]
