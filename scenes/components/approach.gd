81071169c5c3a1ac58fb1574bdd21b7f8c88c677
Guy Geva <guygeva2311@gmail.com>
2026-03-19 16:23:28 -0700

    Experimental: Navigation

--- scenes/components/approach.gd
+++ scenes/components/approach.gd
@@ -1,4 +1,4 @@
-extends Node
+extends NavigationAgent2D
 
 class_name ApproachComponent
 
@@ -11,20 +11,34 @@ var active: bool = false
 var dashing: bool = false
 
 @onready var parent: Node2D = get_parent()
+# @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 signal reached_target(who: Node2D, target: Node2D)
 
-func _process(delta: float) -> void:
-	if target and active:
-		var distance := target.global_position - self.parent.global_position
-		if distance.length() < close_threshold:
-			self.reached_target.emit(self.parent, self.target)
-			# target = null  # TODO?
-			active = false
-		else:
-			var speed = dash_speed if dashing else normal_speed
-			self.parent.position += distance.normalized() * delta * speed
+func _ready():
+	# assert(navigation_agent != null)
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	self.path_desired_distance = 4.0
+	self.target_desired_distance = 4.0
+
 
 func start_chasing(new_target: Node2D) -> void:
 	target = new_target
 	active = true
+	self.set_target_position(new_target.global_position)
+
+func _physics_process(delta):
+	if not target or not active:
+		return
+	if self.is_navigation_finished():
+		active = false
+		self.reached_target.emit(self.parent, self.target)
+		return
+
+	var speed := dash_speed if dashing else normal_speed
+	var next_path_position: Vector2 = self.get_next_path_position()
+	self.parent.global_position = self.parent.global_position.move_toward(next_path_position, speed * delta)
+
+	# self.parent.velocity = current_agent_position.direction_to(next_path_position) * speed
+	# self.parent.move_and_slide()
